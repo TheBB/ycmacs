03ab886e27bd5e7fde5db2320d948cbd1544e770
Eivind Fonn <evfonn@gmail.com>
2014-08-05 13:29:50 +0200

    Safer startup

--- ycmacs.el
+++ ycmacs.el
@@ -24,11 +24,11 @@
 
 (defvar ycm/hmac-secret)
 (defvar ycm/ycmd-process)
-(defvar ycm/ycmd-buffer)
-(defvar ycm/ycmd-port)
+(defvar ycm/ycmacs-buffer)
+(defvar ycm/ycmd-port nil)
 
 (defun ycm/log (str)
-  (with-current-buffer ycm/ycmd-buffer
+  (with-current-buffer ycm/ycmacs-buffer
     (save-excursion
       (goto-char (point-max))
       (insert str))))
@@ -37,7 +37,6 @@
   (catch 'ready
     (when (string-match "\\`serving on http://\\([0-9]+\\.\\)\\{3\\}[0-9]+:[0-9]+" str)
       (setq ycm/ycmd-port (string-to-number (car (last (split-string str ":")))))
-      (ycm/log (format "ycmd running on port %d\n" ycm/ycmd-port))
       (throw 'ready ""))
     (ycm/log (format "--- %s" str))))
 
@@ -61,25 +60,38 @@
 (defun ycm/hello ()
   (interactive)
 
-  (ycm/generate-hmac-secret)
-  (setq ycm/ycmd-buffer (get-buffer-create "*ycmacs*"))
+  (setq ycm/ycmacs-buffer (get-buffer-create "*ycmacs*"))
   (ycm/log "Welcome to ycmacs\n")
 
   (let ((settings (ycm/get-default-settings))
         (temp-file (make-temp-file "ycmacs"))
         (json-object-type 'hash-table))
+
+    ;; Generate an auth key, and save the augmented settings to a temp file
+    (ycm/generate-hmac-secret)
     (puthash "hmac_secret" ycm/hmac-secret settings)
     (with-temp-file temp-file (insert (json-encode settings)))
-    (ycm/log (format "Temporary settings file: %s\n" temp-file))
+
+    ;; Start ycmd and link the process to the filter function
     (let ((process-connection-type t))
-      (setq ycmd-process (start-process
-                          "ycmd"
-                          ycm/ycmd-buffer
-                          ycm/path-to-python2
-                          ycm/path-to-ycmd
-                          (format "--options_file=%s" temp-file)
-                          (format "--idle_suicide_seconds=%d" ycm/server-idle-suicide-seconds))))
-    (set-process-filter ycmd-process 'ycm/ycmd-filter)
-  ))
+      (setq ycm/ycmd-process (start-process
+                              "ycmd"
+                              ycm/ycmacs-buffer
+                              ycm/path-to-python2
+                              ycm/path-to-ycmd
+                              (format "--options_file=%s" temp-file)
+                              (format "--idle_suicide_seconds=%d" ycm/server-idle-suicide-seconds))))
+    (set-process-filter ycm/ycmd-process 'ycm/ycmd-filter)
+
+    ;; Wait until it respons with a port or exits
+    (while (and (not ycm/ycmd-port)
+                (eq 'run (process-status ycm/ycmd-process)))
+      (sleep-for 0 10))
+
+    ;; Make sure the port is set to nil if we got an error
+    (if (and ycm/ycmd-port (eq 'run (process-status ycm/ycmd-process)))
+        (ycm/log (format "ycmd running on port %d\n" ycm/ycmd-port))
+      (ycm/log "ycmd failed to start")
+      (setq ycm/ycmd-port nil))))
 
 (provide 'ycmacs)
